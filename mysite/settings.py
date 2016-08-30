02bb704503628e656227254d4bdf7b86db2b3fe7
ANUHYA BHEEMIREDDY <ANUHYA BHEEMIREDDY>
2016-08-29 20:03:27 -0500

    fourth commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'None'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 
@@ -15,9 +16,13 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
-
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]   
 
 class DetailView(generic.DetailView):
     model = Question
@@ -27,6 +32,11 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 def vote(request, question_id):
